b9e552b8278b6ef78cefbee01ec485afec013cdb
Thingholm <rasmusthingholm@gmail.com>
2024-09-25 20:25:35 +0200

    initial create migration

---
+++ api/WebApi/Migrations/20240925182522_InitialCreate.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "bills",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    bill_tag = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    title = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    question = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    description = table.Column<string>(type: "character varying(2500)", maxLength: 2500, nullable: true),
+                    for_explanation = table.Column<string>(type: "character varying(2500)", maxLength: 2500, nullable: true),
+                    against_explanation = table.Column<string>(type: "character varying(2500)", maxLength: 2500, nullable: true),
+                    link = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_bills", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "parties",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    letter = table.Column<char>(type: "character(1)", nullable: false),
+                    color_hex = table.Column<string>(type: "character varying(7)", maxLength: 7, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_parties", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "votes",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    bill_id = table.Column<int>(type: "integer", nullable: false),
+                    party_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_votes", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_votes_bills_bill_id",
+                        column: x => x.bill_id,
+                        principalTable: "bills",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_votes_parties_party_id",
+                        column: x => x.party_id,
+                        principalTable: "parties",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_votes_bill_id",
+                table: "votes",
+                column: "bill_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_votes_party_id",
+                table: "votes",
+                column: "party_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "votes");
+
+            migrationBuilder.DropTable(
+                name: "bills");
+
+            migrationBuilder.DropTable(
+                name: "parties");
+        }
+    }
+}
