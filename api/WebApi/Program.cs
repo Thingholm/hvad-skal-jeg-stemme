f2a6dc177bcce9a26f4562007f5a9dfef634b1e7
Thingholm <rasmusthingholm@gmail.com>
2024-09-26 17:09:27 +0200

    formatting

--- api/WebApi/Program.cs
+++ api/WebApi/Program.cs
@@ -1,45 +1,44 @@
 using Microsoft.EntityFrameworkCore;
 using WebApi.Data;
 
-namespace WebApi
+namespace WebApi;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var connectionString = builder.Configuration.GetConnectionString("ConnectionString");
+        builder.Services.AddDbContext<AppDbContext>(options => 
+            options
+                .UseNpgsql(connectionString)
+                .UseSnakeCaseNamingConvention()
+        );
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var connectionString = builder.Configuration.GetConnectionString("ConnectionString");
-            builder.Services.AddDbContext<AppDbContext>(options => 
-                options
-                    .UseNpgsql(connectionString)
-                    .UseSnakeCaseNamingConvention()
-            );
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
