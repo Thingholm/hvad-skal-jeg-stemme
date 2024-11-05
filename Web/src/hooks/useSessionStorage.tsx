0472299055d1c6f4dcfaa1f016408e7f7aa65598
Thingholm <rasmusthingholm@gmail.com>
2024-11-05 14:11:20 +0100

    useSessionStorage hook

---
+++ Web/src/hooks/useSessionStorage.tsx
@@ -0,0 +1,34 @@
+export type UseSessionStorage<T> = [
+    () => T | undefined,
+    (value: T) => void,
+    () => void
+];
+
+export function useSessionStorage<T>(key: string): UseSessionStorage<T> {
+    const setSessionStorageItem = (value: T): void => {
+        try {
+            window.sessionStorage.setItem(key, JSON.stringify(value));
+        } catch (error) {
+            console.log(error);
+        };
+    };
+
+    const getSessionStorageItem = (): T | undefined => {
+        try {
+            const item = window.sessionStorage.getItem(key) ?? "";
+            return JSON.parse(item);
+        } catch (error) {
+            console.log(error);
+        };
+    };
+
+    const deleteSessionStorageItem = (): void => {
+        try {
+            window.sessionStorage.removeItem(key);
+        } catch (error) {
+            console.log(error);
+        };
+    };
+
+    return [getSessionStorageItem, setSessionStorageItem, deleteSessionStorageItem]
+}
