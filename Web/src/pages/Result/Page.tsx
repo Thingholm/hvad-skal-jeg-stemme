83d54194e9460c95f4fe07b5923773d00259f229
Thingholm <rasmusthingholm@gmail.com>
2024-11-13 17:25:44 +0100

    retake test btn

--- Web/src/pages/Result/Page.tsx
+++ Web/src/pages/Result/Page.tsx
@@ -1,7 +1,12 @@
+import { useNavigate } from "react-router-dom";
+import Button from "../../components/Button";
 import { Result } from "../../data/getResults";
 import PartyRow from "./PartyRow";
+import { IoRefreshOutline } from "react-icons/io5";
 
 export default function ResultPage(){
+    const navigate = useNavigate();
+
     const results: Result[] = JSON.parse(localStorage.getItem("results") ?? "null");
 
     if (results == null) {
@@ -14,13 +19,14 @@ export default function ResultPage(){
     }
 
     return (
-        <div className="flex-col justify-center items-center my-4 mx-16 py-8 px-16 shadow-xl bg-white  rounded-lg">
-            <h1 className="font-bold text-xl mb-4">Resultat</h1>
+        <div className="relative flex-col justify-center items-center my-4 mx-16 py-8 px-12 shadow-xl bg-white  rounded-lg">
+            <h1 className="font-bold text-xl mb-4 px-4">Resultat</h1>
             {results.sort((a, b) => b.points - a.points).map((result, index) => {
                 return (
                    <PartyRow key={"resultRow" + index} result={result}/>
                 )
             })}
+            <Button type="secondary" className="absolute top-8 right-16 flex items-center" onClick={() => navigate("/test")}><><IoRefreshOutline className="mr-2"/>Tag testen igen</></Button>
         </div>
     )
 
--- Web/src/pages/Result/PartyRow.tsx
+++ Web/src/pages/Result/PartyRow.tsx
@@ -3,6 +3,7 @@ import { Result } from "../../data/getResults"
 import { Question } from "../../data/queryHooks/useQuestions"
 import { useVotesByParty } from "../../data/queryHooks/useVotes"
 import Vote from "../Votes/Vote"
+import { IoChevronDown } from "react-icons/io5"
 
 interface Props {
     result: Result
@@ -19,31 +20,37 @@ export default function PartyRow({ result }: Props){
     }) as Question);
 
     return(
-        <div className="w-full my-2">
-            <div className="w-11/12 hover:cursor-pointer" onClick={() => setIsOpen(state => state ? false : true)}>
-                <h4 
-                    style={{ color: result.party?.colorHex}}
-                    className="mb-1 font-medium"
-                >
-                    {result.party?.letter} - {result.party?.name}
-                </h4>
-                <div 
-                    style={{ 
-                        width: result.agreementRate + "%", 
-                        backgroundColor: result.party?.colorHex
-                    }}
-                    className="relative h-6 rounded-md"
-                >
-                    <span 
+        <div className="w-full">
+            <div 
+                className="group w-full px-4 py-2 flex items-center justify-between rounded-lg duration-150 hover:cursor-pointer hover:bg-gray-200" 
+                onClick={() => setIsOpen(state => state ? false : true)}
+            >
+                <div className="w-11/12">
+                    <h4 
                         style={{ color: result.party?.colorHex}}
-                        className={"absolute left-full font-medium " + (result.agreementRate == 0 ? "" : "pl-2")}
+                        className="mb-1 font-medium"
                     >
-                        {Math.round(result.agreementRate * 10) / 10}%
-                    </span>
+                        {result.party?.letter} - {result.party?.name}
+                    </h4>
+                    <div 
+                        style={{ 
+                            width: result.agreementRate + "%", 
+                            backgroundColor: result.party?.colorHex
+                        }}
+                        className="relative h-6 rounded-md"
+                    >
+                        <span 
+                            style={{ color: result.party?.colorHex}}
+                            className={"absolute left-full font-medium " + (result.agreementRate == 0 ? "" : "pl-2")}
+                        >
+                            {Math.round(result.agreementRate * 10) / 10}%
+                        </span>
+                    </div>
                 </div>
+                <IoChevronDown size={20} className={"inline duration-200 opacity-0 group-hover:opacity-100 " + (isOpen && "rotate-180")}/>
             </div>
 
-            <div className={" " + (isOpen ? "block" : "hidden")}>
+            <div className={"px-4 " + (isOpen ? "block" : "hidden")}>
                 {convertedPartyVote &&
                     convertedPartyVote.map(vote => {
                         return (
